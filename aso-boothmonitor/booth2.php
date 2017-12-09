d73464956d273ec242b25aa1cf3b74c07077f7d6
mapps-fujisawa <yoshitaka.fujisawa@mkt-apps.com>
2017-12-09 16:28:00 +0900

    Add boothmonitor

---
+++ aso-boothmonitor/booth2.php
@@ -0,0 +1,342 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>FY18 Fuyufes Instagrammable Judge by AI presented by ASO</title>
+
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="css/reveal.css">
+		<link rel="stylesheet" href="css/theme/league.css" id="theme">
+
+		<!-- Theme used for syntax highlighting of code -->
+		<link rel="stylesheet" href="lib/css/zenburn.css">
+
+		<!-- Printing and PDF exports -->
+		<script>
+			var link = document.createElement( 'link' );
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = window.location.search.match( /print-pdf/gi ) ? 'css/print/pdf.css' : 'css/print/paper.css';
+			document.getElementsByTagName( 'head' )[0].appendChild( link );
+			setTimeout("location.reload(true)",140000);
+
+		</script>
+
+		<!--[if lt IE 9]>
+		<script src="lib/js/html5shiv.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+		<div class="reveal">
+
+			<!-- Any section element inside of this container is displayed as a slide -->
+			<div class="slides">
+				<section id="front">
+					<h1>FY18 Fuyufes </h1>
+					<h3>Instagrammable Judge AI</h3>
+					<p>
+						<small>
+							Presented by <a href="https://in.accenture.com/japan/aso-page-partial/">ASO</a>
+
+							<br>
+							<br>
+							Transition set<br>
+							<a href="?transition=fade">Fade</a> -
+							<a href="?transition=slide">Slide</a> -
+							<a href="?transition=convex">Convex</a> -
+							<a href="?transition=concave">Concave</a> -
+							<a href="?transition=zoom">Zoom</a>
+							<br>
+							<br>
+							Themes set<br>
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/black.css'); return false;">Black</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/white.css'); return false;">White</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/league.css'); return false;">League</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/sky.css'); return false;">Sky</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/beige.css'); return false;">Beige</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/simple.css'); return false;">Simple</a> <br>
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/serif.css'); return false;">Serif</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/blood.css'); return false;">Blood</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/night.css'); return false;">Night</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/moon.css'); return false;">Moon</a> -
+							<a href="#" onclick="document.getElementById('theme').setAttribute('href','css/theme/solarized.css'); return false;">Solarized</a>
+							
+						</small>
+					</p>
+				</section>
+
+				<!-- 各ランキング表示 -->
+				<section>
+					<section>
+						<h2>Instagrammable Ranking <br>[FOOD section]</h2>
+						<table>
+						<thead>
+							<tr>
+								<th>Rank</th>
+								<th>Name</th>
+								<th>Score</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>動的に取る</td>
+								<td>動的に取る</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>動的に取る</td>
+								<td>動的に取る</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>動的に取る</td>
+								<td>動的に取る</td>
+							</tr>
+						</tbody>
+					</table>
+					</section>
+					<section data-background="https://goo.gl/k4tMpj">
+						<h2><span style="background-color:#FE2E9A">FOOD RANK 1:名前 / スコア</span></h2>
+					</section>
+					<section data-background="https://goo.gl/U3RZRv">
+						<h2><span style="background-color:#FE2E9A">FOOD RANK 2:名前 / スコア</span></h2>
+					</section>
+					<section data-background="https://goo.gl/ViLM9R">
+						<h2><span style="background-color:#FE2E9A">FOOD RANK 3:名前 / スコア</span></h2>
+					</section>
+				</section>
+
+				<section>
+					<section>
+						<h2>Instagrammable Ranking <br>[HUMAN section]</h2>
+						<table>
+							<thead>
+								<tr>
+									<th>Rank</th>
+									<th>Name</th>
+									<th>Score</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>動的に取る</td>
+									<td>動的に取る</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>動的に取る</td>
+									<td>動的に取る</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>動的に取る</td>
+									<td>動的に取る</td>
+								</tr>
+							</tbody>
+						</table>
+					</section>
+					<section data-background="https://goo.gl/k4tMpj">
+						<h2><span style="background-color:#FE2E9A">HUMAN RANK 1:名前 / スコア</span></h2>
+					</section>
+					<section data-background="https://goo.gl/U3RZRv">
+						<h2><span style="background-color:#FE2E9A">HUMAN RANK 2:名前 / スコア</span></h2>
+					</section>
+					<section data-background="https://goo.gl/ViLM9R">
+						<h2><span style="background-color:#FE2E9A">HUMAN RANK 3:名前 / スコア</span></h2>
+					</section>
+				</section>
+
+				<section>
+					<section>
+						<h2>Instagrammable Ranking <br>[FACILITY section]</h2>
+						<table>
+						<thead>
+							<tr>
+								<th>Rank</th>
+								<th>Name</th>
+								<th>Score</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>動的に取る</td>
+								<td>動的に取る</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>動的に取る</td>
+								<td>動的に取る</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>動的に取る</td>
+								<td>動的に取る</td>
+							</tr>
+						</tbody>
+					</table>
+					</section>
+					<section data-background="https://goo.gl/k4tMpj">
+						<h2><span style="background-color:#FE2E9A">FACILITY RANK 1:名前 / スコア</span></h2>
+					</section>
+					<section data-background="https://goo.gl/U3RZRv">
+						<h2><span style="background-color:#FE2E9A">FACILITY RANK 2:名前 / スコア</span></h2>
+					</section>
+					<section data-background="https://goo.gl/ViLM9R">
+						<h2><span style="background-color:#FE2E9A">FACILITY RANK 3:名前 / スコア</span></h2>
+					</section>
+				</section>
+
+				<section>
+					<section>
+						<h2>Career Face Ranking <br>[MD section]</h2>
+						<table>
+							<thead>
+								<tr>
+									<th>1:名前動的</th>
+									<th>2:名前動的</th>
+									<th>3:名前動的</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><img width="270" height="200" data-src="https://goo.gl/k4tMpj" alt="MD RANK 1"></td>
+									<td><img width="270" height="200" data-src="https://goo.gl/U3RZRv" alt="MD RANK 2"></td>
+									<td><img width="270" height="200" data-src="https://goo.gl/ViLM9R" alt="MD RANK 3"></td>
+								</tr>
+							</tbody>
+						</table>
+					</section>
+				</section>
+
+				<section>
+					<section>
+						<h2>Career Face Ranking <br>[SMGR section]</h2>
+						<table>
+							<thead>
+								<tr>
+									<th>1:名前動的</th>
+									<th>2:名前動的</th>
+									<th>3:名前動的</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><img width="270" height="200" data-src="https://goo.gl/k4tMpj" alt="SMGR RANK 1"></td>
+									<td><img width="270" height="200" data-src="https://goo.gl/U3RZRv" alt="SMGR RANK 2"></td>
+									<td><img width="270" height="200" data-src="https://goo.gl/ViLM9R" alt="SMGR RANK 3"></td>
+								</tr>
+							</tbody>
+						</table>
+					</section>
+				</section>
+
+				<!-- ASO部紹介動画 -->
+				<section>
+					<section>
+						<p>
+							ASO部 活動内容紹介<br>
+							僕たちこんな事やってま～す。
+						</p>
+						<a href="#" class="navigate-down">
+							<img width="40" height="65" data-src="https://s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Down arrow">
+						</a>
+					</section>
+					
+					<section data-background-video="img/aso_movie1.mp4" data-background-color="#000000">
+						<div style="background-color: rgba(128, 128, 128, 0.9); color: #FFF; padding: 10px;">
+							<h2>2017/9/2<br>Cruising Party</h2>
+							<a href="#" class="navigate-down">
+								<img width="40" height="65" data-src="https://s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Down arrow">
+							</a>
+							<a href="#" class="navigate-up">
+								<img width="40" height="65" data-src="https://s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Up arrow" style="transform: rotate(180deg); -webkit-transform: rotate(180deg);">
+							</a>
+						</div>
+					</section>
+					<section data-background-video="img/aso_movie2.mp4" data-background-video-loop="loop" data-background-color="#000000">
+						<div style="background-color: rgba(128, 128, 128, 0.9); color: #FFF; padding: 10px;">
+							<h2>2015/11/8<br>Survival Game</h2>
+							<a href="#" class="navigate-down">
+								<img width="40" height="65" data-src="https://s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Down arrow">
+							</a>
+							<a href="#" class="navigate-up">
+								<img width="40" height="65" data-src="https://s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Up arrow" style="transform: rotate(180deg); -webkit-transform: rotate(180deg);">
+							</a>
+						</div>
+					</section>
+					<section data-background-video="img/aso_movie3.mp4?" data-background-video-loop="loop" data-background-color="#000000">
+						<div style="background-color: rgba(128, 128, 128, 0.9); color: #FFF; padding: 10px;">
+							<h2>2016/2/27<br>Night Party @ Sasazuka Bowl</h2>
+							<a href="#" class="navigate-up">
+								<img width="40" height="65" data-src="https://s3.amazonaws.com/hakim-static/reveal-js/arrow.png" alt="Up arrow" style="transform: rotate(180deg); -webkit-transform: rotate(180deg);">
+							</a>
+						</div>
+					</section>
+				</section>
+
+				<!-- 締めのページ -->
+				<section>
+					<h1>Waht's  ASO ?</h1>
+					<p>
+						「楽しいx繋がるxためになる」の<b>3T</b>をモットーに、<br>
+						タテxヨコxナナメのネットワーキングを構築！<br>
+						やってみたい、を企画して楽しみながら！！<br>
+						<br>
+						そんなASO部に興味ある方メールください！<br>
+					<small>
+						mail:<a href="mailto:Nishizawa, Hiroyasu <hiroyasu.nishizawa@accenture.com>">ASO.JP@accenture.com</a>まで
+					</small><br>
+					<span style="text-align: center;">
+						<img width="132" height="132" data-src="img/maillink.png" alt="MailLinkQR">
+					</span>
+					<br>
+					<small>
+						※上記QRを読み込んでメール送信でもOK！
+					</small>
+					</p>
+				</section>
+
+			</div>
+		</div>
+
+		<script src="lib/js/head.min.js"></script>
+		<script src="js/reveal.js"></script>
+
+		<script>
+
+			// More info https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				controls: false,
+				progress: false,
+				history: true,
+				center: true,
+				loop: true,
+				autoSlide: 7000,
+				
+				transition: 'convex', // none/fade/slide/convex/concave/zoom
+
+				// More info https://github.com/hakimel/reveal.js#dependencies
+				dependencies: [
+					{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+					{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+					{ src: 'plugin/search/search.js', async: true },
+					{ src: 'plugin/zoom-js/zoom.js', async: true },
+					{ src: 'plugin/notes/notes.js', async: true }
+				]
+			});
+
+		</script>
+
+	</body>
+</html>
