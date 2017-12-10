21c40c4a4c8f134cd8e0b32f0ed4695330ffe3ac
yoshifuji <yoshi2430jp@gmail.com>
2017-12-10 15:55:00 +0900

    Mod jsonp data transmission

--- aso-boothmonitor/js/retrieve_slidedata.js
+++ aso-boothmonitor/js/retrieve_slidedata.js
@@ -2,42 +2,56 @@
  * Created by yoshi2430jp on 17/12/09.
  */
 $(function(){
-    $("#btnReset").click(function(){
         //params: type, category, subcategory
-        setStaticData(0, 'insta', 'food');
-//    setStaticData(1, 'insta', 'human');
-//    setStaticData(2, 'insta', 'facility');
-//    setStaticData(3, 'face', 'md');
-//    setStaticData(4, 'face', 'smgr');
-    });
+        setStaticData('insta', 'food');
+//    setStaticData('insta', 'human');
+//    setStaticData('insta', 'facility');
+//    setStaticData('face', 'md');
+//    setStaticData('face', 'smgr');
 })
 
 /*
- get images
+ set static contents
 */
-function setStaticData($type, $cat, $sub){
-  $.ajax({
-    type: "POST",
-    url: "./api/retrieve_image.php",
-    data: {
-      "category"     : $cat,
-      "subcategory"  : $sub
-    },
-    success: function(data, dataType){
-      console.log(data);
+function setStaticData(cat, sub){
+
+    //AJAX通信(ver1.8...)
+    var request = $.ajax({
+        type: 'GET', //couldn't use POST in jsonp protocol
+        data:{
+            "category"     : cat,
+            "subcategory"  : sub
+        },
+        url: "https://www.fj-log.com/admin/api/retrieve_static_data.php",
+        async:true,
+        cache:false,
+        dataType: 'jsonp',
+        jsonpCallback: 'jsonp_data',
+        timeout: 3000
+    });
+
+    request.done(function(data, textStatus) {
+        console.log(data);
         var img_prefix = 'https://s3-ap-northeast-1.amazonaws.com/fuyufes2017/img/std/';
 
         for (var i = 0; i < data.length; i++) {
-            $('.name'  + $type + i).text(data[i]["user_name"]);
-            $('.score' + $type + i).text(data[i]["score"]);
-            $('.img'   + $type + i).attr("data-background", img_prefix + data[i]["image_name"]);
+            $('.name_'  + cat + '_' + sub + '_'+ i).text(data[i]["user_name"]);
+            $('.score_' + cat + '_' + sub + '_'+ i).text(data[i]["score"]);
+            $('.img_'   + cat + '_' + sub + '_'+ i).attr("data-background", img_prefix + data[i]["image_name"]);
         }
-      },
-    error: function(XMLHttpRequest, textStatus, errorThrown){
-      alert('Error : ' + errorThrown);
-      $("#XMLHttpRequest").html("XMLHttpRequest : " + XMLHttpRequest.status);
-      $("#textStatus").html("textStatus : " + textStatus);
-      $("#errorThrown").html("errorThrown : " + errorThrown);
-    }
-  });
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+        console.dir("----fail.----");
+        console.dir("fail-jqXHR"+jqXHR);             //オブジェクト
+        console.dir("fail-textStatus:"+textStatus);  //通信ステータス
+        console.dir("fail-errorThrown"+errorThrown); //エラーメッセージ
+    });
+
+    request.always(function(data, textStatus) {
+        console.dir("----always.----");
+        console.dir("always-data:"+data);              //データ
+        console.dir("always-textStatus:"+textStatus);  //通信ステータス
+    });
+
 }
