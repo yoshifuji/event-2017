94e08d1bd19bf5a8b7b7f52a478ec8a84584d443
yoshifuji <yoshi2430jp@gmail.com>
2017-12-10 16:27:13 +0900

    Mod slide js

--- aso-boothmonitor/js/retrieve_slidedata.js
+++ aso-boothmonitor/js/retrieve_slidedata.js
@@ -15,6 +15,14 @@ $(function(){
 */
 function setStaticData(cat, sub){
 
+    var isProduction = location.hostname.match(/prd/) ? 1 : 0;
+    var apiUrl = isProduction ?
+        "https://prod.fj-log.com/admin/api/retrieve_static_data.php":
+        "https://www.fj-log.com/admin/api/retrieve_static_data.php";
+    var imgPrefix = isProduction ?
+        'https://s3-ap-northeast-1.amazonaws.com/prd-fuyufes2017/img/std/':
+        'https://s3-ap-northeast-1.amazonaws.com/fuyufes2017/img/std/';
+
     //AJAX通信(ver1.8...)
     var request = $.ajax({
         type: 'GET', //couldn't use POST in jsonp protocol
@@ -22,7 +30,7 @@ function setStaticData(cat, sub){
             "category"     : cat,
             "subcategory"  : sub
         },
-        url: "https://www.fj-log.com/admin/api/retrieve_static_data.php",
+        url: apiUrl,
         async:true,
         cache:false,
         dataType: 'jsonp',
@@ -32,12 +40,10 @@ function setStaticData(cat, sub){
 
     request.done(function(data, textStatus) {
         console.log(data);
-        var img_prefix = 'https://s3-ap-northeast-1.amazonaws.com/fuyufes2017/img/std/';
-
         for (var i = 0; i < data.length; i++) {
             $('.name_'  + cat + '_' + sub + '_'+ i).text(data[i]["user_name"]);
             $('.score_' + cat + '_' + sub + '_'+ i).text(data[i]["score"]);
-            $('.img_'   + cat + '_' + sub + '_'+ i).attr("data-background", img_prefix + data[i]["image_name"]);
+            $('.img_'   + cat + '_' + sub + '_'+ i).attr("data-background", imgPrefix + data[i]["image_name"]);
         }
     });
 
